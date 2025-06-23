ada40324c2b363812f16c49661731ba4756d39c5
rafael vincent <167981239+codenoob66@users.noreply.github.com>
2025-06-23 14:16:44 +0000

    initial setup mongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,8 @@
 import express from 'express'
+import dontenv from "dotenv"
+import { connectDB } from './config/db.js'
+
+dontenv.config()
 
 const app = express()
 const port = 5000
@@ -8,6 +12,9 @@ app.get('/', (req,res) => {
     res.send('Please exit you are not welcome here')
 })
 
+// console.log(process.env.MONGO_URI)
+
 app.listen(port, () => {
+    connectDB();
     console.log('Server started at http://localhost:5000')
 })
