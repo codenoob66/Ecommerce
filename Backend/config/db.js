1be20872f75c84287377991effd238746d70ecad
rafael vincent <smurf4812@gmail.com>
2025-07-18 16:30:16 +0800

    7/18/25
    working on api

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,15 +1,11 @@
-import mongoose from "mongoose"
-
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(`Encountered an error: ${error.message}`);
-        process.exit(1); // Exit process with failure
-    }
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`Encountered an error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
 };
--- Backend/index.js
+++ Backend/index.js
@@ -1,20 +1,45 @@
-import express from 'express'
-import dontenv from "dotenv"
-import { connectDB } from './config/db.js'
+import express from "express";
+import dontenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
-dontenv.config()
+dontenv.config();
 
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
+app.use(express.json());
 
-app.get('/', (req,res) => {
-    res.send('Please exit you are not welcome here')
-})
+// app.get("/", (req, res) => {
+//   res.send("Please exit you are not welcome here");
+// });
 
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Please provide all fields",
+    });
+  }
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({
+      success: true,
+      data: newProduct,
+    });
+  } catch (error) {
+    console.error("Error in creating proddduct:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+});
 // console.log(process.env.MONGO_URI)
 
 app.listen(port, () => {
-    connectDB();
-    console.log('Server started at http://localhost:5000')
-})
+  connectDB();
+  console.log("Server started at http://localhost:5000");
+});
