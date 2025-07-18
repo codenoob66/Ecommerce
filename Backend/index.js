fb75de48bb4928b7e4523fbe923e4ff3d28ef8a4
rafael vincent <smurf4812@gmail.com>
2025-07-18 19:21:28 +0800

    hey

--- Backend/index.js
+++ Backend/index.js
@@ -39,6 +39,15 @@ app.post("/api/products", async (req, res) => {
 });
 // console.log(process.env.MONGO_URI)
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deeleted" });
+    console.log("product deleted");
+  } catch (error) {}
+});
+
 app.listen(port, () => {
   connectDB();
   console.log("Server started at http://localhost:5000");
