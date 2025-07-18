c4febd8f235a5b580f71c391f3ee9a7a675abda5
rafael vincent <smurf4812@gmail.com>
2025-07-18 23:35:13 +0800

    hey

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dontenv from "dotenv";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+import productRoutes from "./routes/product.route.js";
 
 dontenv.config();
 
@@ -9,44 +9,7 @@ const app = express();
 const port = 5000;
 
 app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("Please exit you are not welcome here");
-// });
-
-app.post("/api/products", async (req, res) => {
-  const product = req.body;
-  if (!product.name || !product.price || !product.image) {
-    return res.status(400).json({
-      success: false,
-      message: "Please provide all fields",
-    });
-  }
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({
-      success: true,
-      data: newProduct,
-    });
-  } catch (error) {
-    console.error("Error in creating proddduct:", error.message);
-    res.status(500).json({
-      success: false,
-      message: "Server error",
-    });
-  }
-});
-// console.log(process.env.MONGO_URI)
-
-app.delete("/api/products/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Product deeleted" });
-    console.log("product deleted");
-  } catch (error) {}
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => {
   connectDB();
