8077b384112b7bb116e91e49d34db4f1853c9d90
rafael vincent <smurf4812@gmail.com>
2025-07-19 12:09:50 +0800

    hey

--- Backend/controllerrs/product.controller.js
+++ Backend/controllerrs/product.controller.js
@@ -33,7 +33,7 @@ export const createProduct = async (req, res) => {
       data: newProduct,
     });
   } catch (error) {
-    console.error("Error in creating proddduct:", error.message);
+    console.error("Error in creating product:", error.message);
     res.status(500).json({
       success: false,
       message: "Server error",
@@ -45,6 +45,11 @@ export const updateProduct = async (req, res) => {
   const { id } = req.params;
 
   const product = req.body;
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).json({
+      success: false,
+      message: "Invalid Product Id",
+    });
   try {
     const updatedProduct = await Product.findByIdAndUpdate(id, product, {
       new: true,
@@ -65,11 +70,25 @@ export const updateProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
   const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId(id)) {
+    return res.status(404).json({
+      success: false,
+      message: "Invalid search",
+    });
+  }
   try {
     await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: "Product deeleted" });
+    res.status(200).json({
+      success: true,
+      message: "Product deleted",
+    });
     console.log("product deleted");
   } catch (error) {
     console.log("error in deleting product", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
   }
 };
--- Backend/index.js
+++ Backend/index.js
@@ -6,12 +6,12 @@ import productRoutes from "./routes/product.route.js";
 dontenv.config();
 
 const app = express();
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/products", productRoutes);
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server started at http://localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
